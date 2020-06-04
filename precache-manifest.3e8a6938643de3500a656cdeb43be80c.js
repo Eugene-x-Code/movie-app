fe66de96a89e9f8ce9974796bab60aa69f7eefee
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2020-06-05 01:09:58 +0300

    Updates

---
+++ precache-manifest.3e8a6938643de3500a656cdeb43be80c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0546bd881074572e41139dcee563dc3",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "bc24c34854be018430d7",
+    "url": "/movie-app/static/css/2.cfae0931.chunk.css"
+  },
+  {
+    "revision": "b38c725ff4ed11e009d3",
+    "url": "/movie-app/static/css/main.9f932323.chunk.css"
+  },
+  {
+    "revision": "bc24c34854be018430d7",
+    "url": "/movie-app/static/js/2.28699bfc.chunk.js"
+  },
+  {
+    "revision": "f4f31e284fcbb03981800cf9b754ac0b",
+    "url": "/movie-app/static/js/2.28699bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38c725ff4ed11e009d3",
+    "url": "/movie-app/static/js/main.da2b32e7.chunk.js"
+  },
+  {
+    "revision": "255bee9b5e55438d0d88",
+    "url": "/movie-app/static/js/runtime-main.4f650522.js"
+  }
+]);
